d3825f55b4a672f5ad41bd3a8fabb115dcaeb374
zaitun-edutech <zaitun-edutech@gmail.com>
2021-04-20 21:42:38 +0800

    commit pertama

---
+++ app/Views/user/index.php
@@ -0,0 +1,8 @@
+<?= $this->extend('templates/index');  ?>
+<?= $this->section('isi');  ?>
+<!-- Content Wrapper -->
+
+<h1>hello </h1>
+<!-- Main Content -->
+<!-- End of Main Content -->
+<?= $this->endSection();  ?>
